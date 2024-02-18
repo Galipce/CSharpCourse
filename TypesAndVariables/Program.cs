6e1c477686a7caa932b67a198bcdf5c6ee840bde
Galip Badem <bademgalip35@hotmail.com>
2024-02-18 21:34:06 +0300

    commir1

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -11,7 +11,7 @@ namespace TypesAndVariables
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Wrold");
-
+            
             Console.ReadLine();
         }
     }
