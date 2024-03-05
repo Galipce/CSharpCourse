2427c573a382d4c2a10eb10e107ecb07504866b5
Galip Badem <bademgalip35@hotmail.com>
2024-03-06 01:15:57 +0300

    coommiittt

---
+++ Constructor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id=1, FirstName="Galip", Lastname="Badem", City="İstanbul"};
+
+            Customer customer2 = new Customer(2, "Ali", "Veli", "İstanbul");
+
+            Console.WriteLine(customer2.FirstName);
+
+            Customer customer3 = new Customer();
+            customer3.FirstName = "Laylay";
+            customer3.Lastname = "Boyboy";
+        }
+
+    }
+
+    class Customer
+    {
+
+        //Default Constructor
+        public Customer()
+        {
+
+        }
+
+        
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName=firstName;
+            Lastname=lastName;
+            City=city;
+            //Console.WriteLine("Yapıcı blok çalıştı");
+                
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Interfaces2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Interfaces2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IPersonManager
+    {
+        void Add();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        
+        public void Add()
+        {
+            //Müşteri Ekleme Kodları
+            Console.WriteLine("Müşteri Eklendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Employee Eklendi");
+        }
+    }
+}
